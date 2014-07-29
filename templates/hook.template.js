bf099131493e53bf10e8325762da577e30bf0543
Mike McNeil <mike@balderdash.co>
2014-07-28 22:01:59 -0500

    intitial commit

---
+++ templates/hook.template.js
@@ -0,0 +1,14 @@
+/**
+ * <%= filename %> hook
+ */
+
+module.exports = function (sails) {
+  return {
+
+    // Run when sails loads-- be sure and call `next()`.
+    initialize: function (next) {
+      return next();
+    }
+
+  };
+};
